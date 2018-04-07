a2d287819ca7204104191392f54881c069e4bb4e
wintercoder <wintercoder@gmail.com>
2018-04-07 16:57:35 +0800

    first commit

---
+++ gensql.php
@@ -0,0 +1,325 @@
+<?php
+
+class ValueGenerator{
+
+    public function __call($name, $arguments){
+        echo "生成规则 $name 不存在\n";exit();
+    }
+
+    /**
+     * 日期自增，全局自增模式 
+     * @param $input array
+     *      从 from值 开始自增 格式: 20180301
+     * @param $groupSize int 每组多少条合并成一个数组，也是该次函数返回的数组大小
+     * @param $sqlCounter int  当前是第几条SQL，用于在多条SQL之间仍然保持自增
+     * @return array
+     */
+    public function incrDay($input,$groupSize,$sqlCounter)
+    {
+        $begin = $input;
+        $offset = ($sqlCounter - 1) * $groupSize;
+        $ret = []; $count = 0;
+
+        $begin = date('Ymd',strtotime("{$begin} + {$offset} day"));
+        while ($count < $groupSize) {
+            $ret []= date('Ymd',strtotime("{$begin} + {$count} day"));
+            $count++;
+        }
+        return $ret;
+    }
+
+    /**
+     * 日期自增，组递增模式，每组的日期相同  
+     * @param $input array
+     *      从 from值 开始自增 格式: 20180301
+     * @param $groupSize int 每组多少条合并成一个数组，也是该次函数返回的数组大小
+     * @param $sqlCounter int  当前是第几条SQL，用于在多条SQL之间仍然保持自增
+     * @return array
+     */
+    public function incrDayGrouply($input,$groupSize,$sqlCounter)
+    {
+        $begin = $input;
+        $offset = ($sqlCounter - 1) ;
+        $ret = []; $count = 0;
+
+        $begin = date('Ymd',strtotime("{$begin} + {$offset} day"));
+        while ($count < $groupSize) {
+            $ret []= date('Ymd',strtotime("{$begin} + 0 day"));
+            $count++;
+        }
+        return $ret;
+    }
+    /**
+     * 自增int
+     * @param $input array
+     *      从 from值 开始自增
+     * @param $groupSize int 每组多少条合并成一个数组，也是该次函数返回的数组大小
+     * @param $sqlCounter int  当前是第几条SQL，用于在多条SQL之间仍然保持自增
+     * @return array
+     */
+    public function incrInt($input,$groupSize,$sqlCounter)
+    {
+        $begin = intval($input);
+        $offset = ($sqlCounter - 1) * $groupSize;
+        $ret = []; $count = 0;
+        $begin += $offset;
+        while ($count < $groupSize) {
+            $ret []= $begin++;
+            $count++;
+        }
+        return $ret;
+    }
+    /**
+     * 随机int
+     * @param $input string
+     *      闭区间生成 [from,to]
+     * @param $groupSize int 每组多少条合并成一个数组，也是该次函数返回的数组大小
+     * @return array
+     */
+    public function randInt($input,$groupSize)
+    {
+        $exp = explode(',',$input);
+        $from = !empty($exp[0]) ? intval($exp[0]) : 1;
+        $to = !empty($exp[1]) ? intval($exp[1]) : 100;
+        $ret = [];
+        $count = 0;
+        while ($count++ < $groupSize) {
+            $ret []= mt_rand($from,$to);
+        }
+        return $ret;
+    }
+
+    /**
+     * 指定日期里的 随机时间戳
+     * @param $input string
+     *      [from,to] 中的时间戳
+     * @param $groupSize int 每组多少条合并成一个数组，也是该次函数返回的数组大小
+     * @return array
+     */
+    public function randTimestamp($input,$groupSize)
+    {
+        $exp = explode(',',$input);
+        $from = !empty($exp[0]) ? intval($exp[0]) : 20180401;
+        $to = !empty($exp[1]) ? intval($exp[1]) : 20180404;
+        $diff = strtotime($to) - strtotime($from);
+        $ret = [];
+        $count = 0;
+        while ($count++ < $groupSize) {
+            $ret []= strtotime($from) + mt_rand(0,$diff);
+        }
+        return $ret;
+    }
+
+    /**
+     * 常量
+     * @param $input
+     *          value 常量值
+     * @param $groupSize int 每组多少条合并成一个数组，也是该次函数返回的数组大小
+     * @return array
+     */
+    public function constStr($input,$groupSize){
+        return explode(',',str_repeat($input.',',$groupSize));
+    }
+
+
+    /**
+     * 常量列表
+     * @param $input
+     *          value 常量值
+     * @param $groupSize int 每组多少条合并成一个数组，也是该次函数返回的数组大小
+     * @return array
+     */
+    public function constStrList($input,$groupSize){
+        $input = !empty($input) ? $input : '百度,阿里,腾讯';
+        return explode(',',$input);
+    }
+
+    /**
+     * 随机字符串（字符集：大小写字母）
+     * @param $input array
+     *          length 长度
+     * @param $groupSize int 每组多少条合并成一个数组，也是该次函数返回的数组大小
+     * @return array
+     */
+    public function randStr($length,$groupSize)
+    {
+        $ret = [];
+        $count = 0;
+        while ($count++ < $groupSize) {
+            $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+            $ranStr = '';
+            for ($i = 0; $i < $length; $i++) {
+                $ranStr .= $characters[rand(0, strlen($characters) - 1)];
+            }
+            $ret []= $ranStr;
+        }
+        return $ret;
+    }
+    /**
+     * 在字符串列表里 随机，全局可重复
+     * @param $input array
+     *          length 长度
+     * @param $groupSize int 每组多少条合并成一个数组，也是该次函数返回的数组大小
+     * @return array
+     */
+    public function randStrList($input,$groupSize)
+    {
+        $input = !empty($input) ? $input : 'ofo,mobike';
+        $value = explode(',',$input);
+        $ret = [];
+        $count = 0;
+        while ($count++ < $groupSize) {
+            $ranStr = $value[rand(0, count($value) - 1)];
+            $ret []= $ranStr;
+        }
+        return $ret;
+    }
+
+
+    /**
+     * 自增中文，根据 前缀+1 这种，如程序员1，程序员2
+     * @param $input array
+     *          pre_str 前缀
+     * @param $groupSize int 每组多少条合并成一个数组，也是该次函数返回的数组大小
+     * @return array
+     */
+    public function incrStrPrefix($input,$groupSize,$sqlCounter)
+    {
+        $pre = !empty($input) ? $input : '测试店';
+        $inputArr['from'] = 1;
+        $intArr = $this->incrInt($inputArr,$groupSize,$sqlCounter);
+        $ret = [];
+        foreach ($intArr as $intVal){
+            $str = $pre . $intVal;
+            $ret []= $str;
+        }
+        return $ret;
+    }
+
+    /**
+     * 随机图片，目前是 http://lorempixel.com/
+     * 也可以考虑用百度的 http://image.baidu.com/channel/listjson?pn=0&rn=30&tag1=%E7%BE%8E%E5%A5%B3&tag2=%E5%85%A8%E9%83%A8&ie=utf8
+     * @param $input
+     * @param $groupSize int 每组多少条合并成一个数组，也是该次函数返回的数组大小
+     * @return array
+     */
+    public function randPicUrl($input,$groupSize){
+        $exp = explode(',',$input);
+        $width = !empty($exp[0]) ? intval($exp[0]) : 300;
+        $height = !empty($exp[1]) ? intval($exp[1]) : 300;
+        $url = "http://lorempixel.com/{$width}/{$height}/";
+
+        return explode(',',str_repeat($url.',',$groupSize));
+    }
+}
+
+class WorkHandler{
+
+    public function checkParams($input){
+        $input = json_decode($input,true);  //json转数组
+        if(empty($input) || empty($input['list'])){
+            echo "参数不是JSON格式";exit();
+        }
+        $input['count'] = intval($input['count']);
+        $input['group_size'] = intval($input['group_size']);
+        if( $input['count'] <= 0 || $input['group_size'] <= 0
+            || $input['count'] >= 5000 || $input['group_size'] >= 5000) {
+            echo "条数、组数只能是 1 到 5000 以内";exit();
+        }
+    }
+
+    public function execute($input){
+        $this->checkParams($input);
+
+        $input = json_decode($input,true);  //json转数组
+        $inputList = $input['list'];
+
+        //对于自增ID等忽略的类型，删除它并重排下标，不能用 array_splice ，删除多个时会乱
+        foreach ($inputList as $key => $item){
+            if($item['method'] == 'ignore'){
+                unset($inputList[$key]);
+            }
+        }
+        $inputList = array_values($inputList);  //从0开始 重建下标
+
+        $genCount = $input['count'];
+        $tableName = $input['table_name'];
+        $groupSize = $input['group_size'];              //每个SQL有多少value组
+
+        $keyArr = array_column($inputList,'key');
+
+        $generator = new ValueGenerator();
+
+        $sql = '';
+        //insert的SQL条数
+        for($genI = 1; $genI <= $genCount; $genI++) {
+
+            $genResult = [];    //生成结果数组： key => 生成方法+下标 防止同样的方法覆盖数据，value => 按该方法生成的数据数组
+            foreach ($inputList as $itemIndex => $item){
+                $genService = $this->camelize($item['method']); //方法名转驼峰
+                $genResult [ $item['method'].$itemIndex ] = $generator->$genService($item['value'],$groupSize,$genI );
+            }
+            $valueStr = '';
+
+            //每个SQ有多少value组
+            for($cnt = 0; $cnt < $groupSize; $cnt++){
+
+                $valueStr .= '(';
+                //取每个字段的值，单引号引起来
+                foreach ($genResult as $result) {
+                    $valueStr .= "'{$result[$cnt]}',";
+                }
+                $valueStr = rtrim($valueStr,',');
+                $valueStr .= '),';
+            }
+            $valueStr = rtrim($valueStr,',');
+            $valueStr .= ';';
+            $sql .= "INSERT INTO {$tableName} (" . implode(",",$keyArr) .") VALUES $valueStr";
+            $sql .= "\n";
+            $sql .= "\n";
+        }
+        echo $sql;
+    }
+
+    /**
+     * 下划线转驼峰
+     * @param $str
+     * @param string $separator
+     * @return string
+     */
+    private function camelize($str,$separator='_')
+    {
+        $str = $separator. str_replace($separator, " ", strtolower($str));
+        return ltrim(str_replace(" ", "", ucwords($str)), $separator );
+    }
+}
+
+
+date_default_timezone_set("Asia/Shanghai");
+
+$postData = file_get_contents('php://input');   //提交的是JSON，不能直接$post获取
+if(empty($postData)){
+    echo "我是空白";
+    exit();
+}
+//echo ($postData);exit();
+
+
+$handler = new WorkHandler();
+$handler->execute($postData);
+exit();
+
+/*
+$parser = new CreateSqlParser();
+$ret = $parser->execute($sql);
+if(0 == ($ret['error'])){
+
+    $input = json_encode($ret['data']);
+    $handler = new WorkHandler();
+    $handler->execute($input);
+} else{
+    echo json_encode($ret);exit();
+
+}
+
+*/
---
+++ sqlparse.php
@@ -0,0 +1,218 @@
+<?php
+
+class CreateSqlParser
+{
+
+    const RAND_INT = 'rand_int';
+    const RAND_FLOAT = 'rand_float';
+    const INCR_INT = 'incr_int';
+    const INCR_DAY = 'incr_day';
+    const INCR_DAY_GROUPLY = 'incr_day_grouply';
+    const RAND_TIMESTAMP = 'rand_timestamp';
+    const IGNORE = 'ignore';
+
+    const INCR_STR_PREFIX = 'incr_str_prefix';
+    const RAND_STR = 'rand_str';
+    const RAND_STR_LIST = 'rand_str_list';
+    const CONST_STR = 'const_str';
+    const CONST_STR_LIST = 'const_str_list';
+    const RAND_PIC_LIST = 'rand_pic_url';
+
+
+    const errorParseError = 1001;
+
+    public function getApiReturn($error, $msg, $data)
+    {
+        $ret['error'] = $error;
+        $ret['msg'] = $msg;
+        $ret['data'] = $data;
+        return $ret;
+    }
+
+    public function execute($input)
+    {
+        $sql = $input;
+        $ret = [];
+
+        //解析表名
+        $pattern = "#CREATE TABLE `(.+)` #i";
+        preg_match($pattern, $sql, $matches);
+        if (empty($matches)) {
+            return $this->getApiReturn(self::errorParseError, '不是建表SQL，未包含 CREATE TABLE', []);
+        }
+        $ret['table_name'] = $matches[1];
+
+        //解析字段
+        $pattern = "#`(.+)` (.+)\((.+)\)(.+)#i";
+        preg_match_all($pattern, $sql, $matches);
+        $ret['list'] = [];
+        if (empty($matches[0])) {
+            return $this->getApiReturn(self::errorParseError, '未查找到SQL字段', []);
+        }
+//                echo json_encode($matches); exit();
+
+        //解析后拿到
+        // match[1]: 字段名
+        // match[2]: 类型 (int)
+        // match[3]: 长度 int(10)中的10
+        // match[4]: 其他，NOT NULL AUTO_INCREMENT COMMENT '我是注释' 这种
+        for ($cnt = 0; $cnt < count($matches[0]); $cnt++) {
+            $item = $this->genDefaultAttribute($matches[1][$cnt], $matches[2][$cnt], $matches[3][$cnt], $matches[4][$cnt]);
+            $item['key'] = $matches[1][$cnt];
+            $ret['list'] [] = $item;
+        }
+        $ret['group_size'] = 5;     //组大小
+        $ret['count'] = 3;          //多少条SQL
+
+        return $this->getApiReturn(0, '', $ret);
+    }
+
+    /**
+     * @param $key
+     * @param $type string SQL的字段类型 varchar,int
+     * @param $size  string SQL的字段类型后跟随的大小 如 varchar(10) 中的10
+     * @param $others string  varchar(10) 后面的一串其他完整内容，包含 自增、非空、默认值等
+     * @return array item 必备包含 'key' 字段名 'method' 生成规则 'value' 默认值
+     */
+    private function genDefaultAttribute($key, $type, $size, $others)
+    {
+        $item = [];
+        $type = trim($type);
+        $incrStrPre = ['老王', '射击狮', '测试店', '产品经理', '程序员', '码农', '攻城狮', 'SB'];
+
+        switch ($type) {
+            case 'varchar':
+                $item = [
+                    'desc' => '前缀+自增',
+                    'method' => self::INCR_STR_PREFIX,
+                    'value' => $incrStrPre[rand(0, count($incrStrPre) - 1)],
+                ];
+                break;
+            case 'int':
+                $item = [
+                    'desc' => '随机整数',
+                    'method' => self::RAND_INT,
+                    'value' => '100,500',
+                ];
+                break;
+            case 'bigint':
+                $item = [
+                    'desc' => '随机整数',
+                    'method' => self::RAND_INT,
+                    'value' => "1000000,99999999",
+                ];
+                break;
+            case 'tinyint':
+                $item = [
+                    'desc' => '随机整数',
+                    'method' => self::RAND_INT,
+                    'value' => "0,{$size}",       //tinyint(4) 一般是0-4的枚举值
+                ];
+                break;
+            case 'float':
+                $item = [
+                    'desc' => '随机浮点',
+                    'method' => self::RAND_FLOAT,
+                    'value' => '0,255',
+                ];
+                break;
+        }
+
+        //自增ID
+        $autoInc = stripos($others, "AUTO_INCREMENT");
+        if ($autoInc) {
+            $item = [
+                'method' => self::IGNORE,
+                'desc' => '自增ID，忽略',
+            ];
+        }
+
+        //注释
+        $expComment = explode("COMMENT ", $others);
+        if (!empty($expComment)) {
+            $item['comment'] = trim($expComment[1], "',");
+        }
+
+        //通用配置
+        $commonItem = $this->parseFileForAttribute('conf/common.ini',$key);
+        if(!empty($localItem)){
+            $item = $commonItem;
+        }
+
+        //个性化配置
+        //针对你、你公司 常用的字段设置默认值，存放不可告人的数据秘密，配置文件在.gitignore里
+        $localItem = $this->parseFileForAttribute('conf/local.ini',$key);
+        if(!empty($localItem)){
+            $item = $localItem;
+        }
+        return $item;
+    }
+
+    /**
+     * 解析ini文件拿到自定义默认值，根据字段名猜测用户想要的是哪个类型，配置文件样例如下，目前只支持 模糊查找 和 精确匹配
+            [0]
+            key = avatar
+            method = RAND_PIC_LIST
+            value = 300,400
+            way = search  跟key的匹配方式，search为模糊搜索，输入key包含 avatar 就走这个匹配
+
+            [1]
+            key = index_day
+            method = INCR_DAY
+            value = 20180301
+            way = match  match为精确匹配，输入key 等于 index_day 就走这个匹配
+     *
+     *
+     * @param $fileName string 配置文件路径
+     * @param $key string 字段名
+     * @return array
+     */
+    private function parseFileForAttribute($fileName,$key)
+    {
+        if(!file_exists($fileName)){
+            return [];
+        }
+        $item = [];
+        $matchArray = parse_ini_file($fileName,true);
+
+        foreach ($matchArray as $match) {
+            if(empty($match['way'])){   //默认key规则为相等
+                $match['way'] = 'match';
+            }
+            if($match['way'] == 'search' && false !== stripos($key, $match['key'])){
+                $item = $match;
+                $item['method'] = strtolower($item['method']);
+            }else if ($match['way'] == 'match' && $key == $match['key']) {
+                $item = $match;
+                $item['method'] = strtolower($item['method']);
+            }
+        }
+        unset($item['way']);
+        return $item;
+    }
+
+
+}
+date_default_timezone_set("Asia/Shanghai");
+
+
+$defaultSql = "
+CREATE TABLE `test` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `supplier_id` int(11) NOT NULL DEFAULT '0',
+  `content` varchar(255) COLLATE utf8_bin NOT NULL,
+  `create_time` int(11) NOT NULL,
+  `insert_day` int(11) NOT NULL,
+  `is_deleted` tinyint(2) NOT NULL,
+  `up_count` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;";
+
+
+$sql = $_POST['sql'];
+$parser = new CreateSqlParser();
+if(empty($sql)){
+    $sql = $defaultSql;
+}
+$ret = $parser->execute($sql);
+echo json_encode($ret);exit();
